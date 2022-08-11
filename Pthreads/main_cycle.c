c098b8044b03c185d68d2e52edf85ea6ddb9f2dd
Zach Han <hanzemeng2001518@gmail.com>
2022-08-10 23:24:35 -0400

    added cycle assignment

--- Pthreads/main_cycle.c
+++ Pthreads/main_cycle.c
@@ -13,10 +13,9 @@ typedef struct
 typedef struct
 {
     int start;
-    int end;
     float* return_cost;
     int* return_prev;
-} solve_input;
+} solve_wrapper_input;
 typedef struct
 {
     float cost;
@@ -26,11 +25,15 @@ typedef struct
 float dist(const Point* a, const Point* b);
 double elapsed_seconds();
 
-void setup(const char** path);
-void base_case_calculation();
+void setup(const char** path); // read in input
+void base_case_calculation(); // setup dp and all_path
+void dispatch_thread_work(float* thread_return_cost, int* thread_return_prev);
+// every thread's best path is written to thread_return_cost
+// every thread's best path's second to last vertex is written to thread_return_prev
+void collect_thread_result(float* thread_return_cost, int* thread_return_prev, float* res_cost, int* res_path);
 
-#define MAX_INT 16777216
-#define INT_DIGIT 24
+#define INT_DIGIT 24 // max number of vertices - 1
+#define MAX_INT 16777216 // same as 2^INT_DIGIT
 #define LARGE_VALUE 666666;
 
 unsigned int all_path = 0;
@@ -67,16 +70,16 @@ float solve(unsigned int path, int end)
 }
 void* solve_wrapper(void* parameter)
 {
-    solve_input* input = (solve_input*) parameter;
-    unsigned int mask = 0x00000001;
-    mask <<= (input->start-1);
-
+    solve_wrapper_input* input = (solve_wrapper_input*) parameter;
     float thread_min = LARGE_VALUE;
     int thread_prev;
-    for(int i=input->start; i<input->end; i++)
+
+    for(int i=input->start; i<n; i+=thread_count)
     {
+        unsigned int mask = 0x00000001;
+        mask <<= (i-1);
         unsigned int new_path = all_path & (~mask);
-        mask <<= 1;
+
         float temp = solve(new_path, i) + dist(&points[i], &points[0]);
         if(thread_min>temp)
         {
@@ -84,6 +87,7 @@ void* solve_wrapper(void* parameter)
             thread_prev = i;
         }
     }
+
     *(input->return_cost) = thread_min;
     *(input->return_prev) = thread_prev;
     return NULL;
@@ -98,60 +102,29 @@ int main(const int agrc, const char** argv)
     }
     setup(argv);
     base_case_calculation();
+    
+    float thread_return_cost[thread_count];
+    int thread_return_prev[thread_count];
+    float res_cost = LARGE_VALUE;
+    int res_path[n];
 
     double start = elapsed_seconds();
-    pthread_t thread_handles[thread_count];
-    solve_input thread_inputs[thread_count];
-    float thread_cost[thread_count];
-    int thread_prev[thread_count];
-
-    int k = (n-1)%thread_count;
-    int s = (n-1)/thread_count;
-    for(int i=0; i<thread_count; i++)
-    {   
-        thread_inputs[i].return_cost = &thread_cost[i];
-        thread_inputs[i].return_prev = &thread_prev[i];
-        if(i<k)
-        {
-            thread_inputs[i].start = i*s+i+1;
-            thread_inputs[i].end = thread_inputs[i].start+s+1;
-        }
-        else
-        {
-            thread_inputs[i].start = i*s+k+1;
-            thread_inputs[i].end = thread_inputs[i].start+s;
-        }
-        //printf("%d, %d\n", thread_inputs[i].start,thread_inputs[i].end);
-        pthread_create(&thread_handles[i], NULL, solve_wrapper, (void*) &thread_inputs[i]);
-    }
-
-    for(int i=0; i<thread_count; i++)
-    {
-        pthread_join(thread_handles[i], NULL);
-    }
-    float res_cost = LARGE_VALUE;
-    int res_prev;
-    for(int i=0; i<thread_count; i++)
-    {
-        if(res_cost>thread_cost[i])
-        {
-            res_cost = thread_cost[i];
-            res_prev = thread_prev[i];
-        }
-    }
-    while(res_prev != 0)
-    {
-        printf("%d, ", res_prev);
-        unsigned int mask = 0x00000001;
-        mask <<= (res_prev-1);
-        all_path &= ~mask;
-        res_prev = dp[all_path][res_prev].prev_end;
-    }
-    printf("0\n");
+    dispatch_thread_work(thread_return_cost, thread_return_prev);
+    collect_thread_result(thread_return_cost, thread_return_prev, &res_cost, res_path);
     double end = elapsed_seconds();
-    printf("Cost: %f\n", res_cost);
-    printf("Time: %f\n", end-start);
 
+    float res_cost_actual = dist(&points[res_path[n-1]], &points[res_path[0]]);
+    printf("Path: 0, ");
+    for(int i=1; i<n; i++)
+    {
+        printf("%d, ", res_path[i]);
+        res_cost_actual += dist(&points[res_path[i-1]], &points[res_path[i]]);
+    }
+
+    printf("\n");
+    printf("Computed Cost: %f\n", res_cost);
+    printf("Actual   Cost: %f\n", res_cost_actual);
+    printf("Time Taken(s): %f\n", end-start);
     free(points);
     return 0;
 }
@@ -165,6 +138,11 @@ void setup(const char** input_parameter)
 
     fgets(input_line, 50, filePointer);
     n = strtol(input_line, NULL, 10);
+    if(INT_DIGIT+1<n)
+    {
+        printf("Too many points.\n");
+        exit(1);
+    }
     points = malloc(n*sizeof(Point));
 
     for(int i=0; i<n; i++)
@@ -194,6 +172,47 @@ void base_case_calculation()
         mask <<= 1;
     }
 }
+void dispatch_thread_work(float* thread_return_cost, int* thread_return_prev)
+{
+    pthread_t thread_handles[thread_count];
+    solve_wrapper_input thread_inputs[thread_count];
+
+    for(int i=0; i<thread_count; i++)
+    {   
+        thread_inputs[i].return_cost = &thread_return_cost[i];
+        thread_inputs[i].return_prev = &thread_return_prev[i];
+        thread_inputs[i].start = i+1;
+        pthread_create(&thread_handles[i], NULL, solve_wrapper, (void*) &thread_inputs[i]);
+    }
+    for(int i=0; i<thread_count; i++)
+    {
+        pthread_join(thread_handles[i], NULL);
+    }
+}
+void collect_thread_result(float* thread_return_cost, int* thread_return_prev, float* res_cost, int* res_path)
+{
+    int res_prev;
+    for(int i=0; i<thread_count; i++)
+    {
+        if(*res_cost>thread_return_cost[i])
+        {
+            *res_cost = thread_return_cost[i];
+            res_prev = thread_return_prev[i];
+        }
+    }
+    
+    int index=n-1;
+    while(res_prev != 0)
+    {
+        res_path[index--] = res_prev;
+        unsigned int mask = 0x00000001;
+        mask <<= (res_prev-1);
+        all_path &= ~mask;
+        res_prev = dp[all_path][res_prev].prev_end;
+    }
+    res_path[index--] = 0;
+}
+
 float dist(const Point* a, const Point* b)
 {
     float dif_x = (*a).x-(*b).x;
